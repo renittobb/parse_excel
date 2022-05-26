49399082e77dcf423e7b33937cf2cf6e41401e78
renittojose <renitto.jose@bigbasket.com>
2022-05-26 10:26:17 +0530

    Some refactoring

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,39 +2,28 @@ import 'dart:core';
 
 import 'package:excel/excel.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
   getFileFromLocal() async {
     final result = await FilePicker.platform
         .pickFiles(type: FileType.any, allowMultiple: false);
-    var fileBytes;
+    var fileBytes, fileName;
     if (result?.files.first != null) {
       fileBytes = result?.files.first.bytes;
-      var fileName = result?.files.first.name;
+      fileName = result?.files.first.name;
     }
 
-    var excel = Excel.decodeBytes(fileBytes!);
-    int j = 0;
+    print('File name: $fileName');
 
-    Map<int, List<Data?>> map = Map<int, List<Data?>>();
-
-    for (var table in excel.tables.keys) {
-      for (var row in excel.tables[table]!.rows) {
-        map[++j] = row;
-      }
-    }
-
-    //all the rows here
-    print(map);
+    getDataFromExcel(fileBytes);
   }
 
-  getDataFromExcel() async {
-    ByteData data =
-        await rootBundle.load("/Users/renittojose/Downloads/users.xlsx");
-    var bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-    var excel = Excel.decodeBytes(bytes);
+  getDataFromExcel(var fileBytes) async {
+    // ByteData data =
+    //     await rootBundle.load("/Users/renittojose/Downloads/users.xlsx");
+    // var bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+    var excel = Excel.decodeBytes(fileBytes);
     int j = 0;
 
     Map<int, List<Data?>> map = Map<int, List<Data?>>();
@@ -47,5 +36,10 @@ class HomeController extends GetxController {
 
     //all the rows here
     print(map);
+
+    //get a row - headers - sample
+    map[1]?.forEach((element) {
+      print(element?.value);
+    });
   }
 }
