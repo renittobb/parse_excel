ac9dc7440d7937d345dab087748d4f312d369792
renittojose <renitto.jose@bigbasket.com>
2022-05-26 10:40:32 +0530

    Some more refactoring

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -10,12 +10,10 @@ class HomeController extends GetxController {
         .pickFiles(type: FileType.any, allowMultiple: false);
     var fileBytes, fileName;
     if (result?.files.first != null) {
-      fileBytes = result?.files.first.bytes;
       fileName = result?.files.first.name;
+      fileBytes = result?.files.first.bytes;
     }
-
     print('File name: $fileName');
-
     getDataFromExcel(fileBytes);
   }
 
@@ -25,21 +23,19 @@ class HomeController extends GetxController {
     // var bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
     var excel = Excel.decodeBytes(fileBytes);
     int j = 0;
-
     Map<int, List<Data?>> map = Map<int, List<Data?>>();
-
     for (var table in excel.tables.keys) {
       for (var row in excel.tables[table]!.rows) {
         map[++j] = row;
       }
     }
-
     //all the rows here
     print(map);
-
-    //get a row - headers - sample
-    map[1]?.forEach((element) {
-      print(element?.value);
-    });
+    //get row data - sample
+    for(int i=0;i<map.length;i++) {
+      map[i]?.forEach((element) {
+        print(element?.value);
+      });
+    }
   }
 }
